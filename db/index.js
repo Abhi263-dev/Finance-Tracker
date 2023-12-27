07d8b1033fba5d041795fe028a287671dd949136
Abhishek Dhangar <abhishek.dhangar@cubastion.com>
2023-12-27 11:36:37 +0530

    Initialised express and sequelize database

---
+++ db/index.js
@@ -0,0 +1,34 @@
+const { Sequelize, DataTypes } = require('sequelize');
+
+const mysql = require("mysql");
+
+const sequelize = new Sequelize('ftracker','root','',{
+    host: "localhost",
+    dialect: 'mysql',   //database name in phpmyAdmin through Xampp
+   logging: false
+});
+
+sequelize.authenticate()
+.then(()=>{
+    console.log("connected");
+})
+.catch(err=>{
+    console.log("error"+err)
+})
+
+const db={}
+
+db.sequelize=sequelize
+db.Sequelize = Sequelize
+// db.user=require('../src/models/user')(sequelize, DataTypes)
+
+
+sequelize.sync()
+  .then(() => {
+  })
+  .catch((error) => {
+    console.error('Error syncing database:', error);
+  });
+
+
+module.exports = db;
---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const userRouter = require('./routers/user')
+const db = require('../db/index')
+
+const app=express()
+
+const port=process.env.PORT||3000
+
+app.use(express.json())
+//app.use(userRouter)
+
+
+app.listen(port,()=>{
+    console.log("Connected")
+})
